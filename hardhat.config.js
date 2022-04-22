df94e031591f2d94ad9282343ee2c31dedd514c2
ashhanai <ashhab.naim@gmail.com>
2022-04-23 00:22:40 +0200

    Define .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    kovan: {
+      url: process.env.KOVAN_URL || "",
+      accounts:
+        process.env.DEPLOY_PRIVATE_KEY_TESTNET !== undefined
+          ? [process.env.DEPLOY_PRIVATE_KEY_TESTNET]
+          : [],
+    }
+  },
 };
