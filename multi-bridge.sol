4f2307f20be3fd8ea16f4e6d3ebb4610ebf31a83
shafu.eth <selfouly@gmail.com>
2022-04-22 22:21:25 +0200

    Update multi-bridge.sol

--- multi-bridge.sol
+++ multi-bridge.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+struct DepositData { 
+   address _l1Token;
+    address _l2Token;
+    address _to;
+    uint256 _amount;
+    uint32 _l2Gas;
+    bytes _data;
+}
+
 interface Bridge {
     function depositERC20To(
         address _l1Token,
@@ -11,6 +20,7 @@ interface Bridge {
         bytes calldata _data
     ) external ;
 }
+
 
 
 contract MutliCaller {
@@ -27,27 +37,17 @@ contract MutliCaller {
         } 
     }
 
-    function toUint256(bytes memory _bytes)   
-        internal
-        pure
-        returns (uint256 value) {
-
-            assembly {
+    function toUint256(bytes memory _bytes) internal pure returns (uint256 value) {
+        assembly {
             value := mload(add(_bytes, 0x20))
-            }
         }
+    }
 
-        function toUint32(bytes memory _bytes)   
-        internal
-        pure
-        returns (uint32 value) {
-
-            assembly {
+    function toUint32(bytes memory _bytes) internal pure returns (uint32 value) {
+        assembly {
             value := mload(add(_bytes, 0x04))
-            }
         }
-
-    
+    }
 
     // each token needs to be approved by the bridge first.
     // input: array of array
@@ -60,17 +60,17 @@ contract MutliCaller {
     // - uint256 _amount,
     // - uint32 _l2Gas,
     // - bytes calldata _data
-    function multiBridge(bytes[][] calldata _data) external {
-        for (uint i = 0; i < _data.length; i++) {
-            bytes[] memory callData = _data[i];
+    function multiBridge(DepositData[] calldata _data) external {
+        for (uint i = 0; i < _data.length; ++i) {
+            DepositData depositData = _data[i];
 
             bridge.depositERC20To(
-                bytesToAddress(callData[0]),
-                bytesToAddress(callData[1]),
-                bytesToAddress(callData[2]),
-                toUint256(callData[3]),
-                toUint32(callData[4]),
-                callData[5]);        
+                depositData._l1Token,
+                depositData._l2Token,
+                msg.sender,
+                depositData._amount,
+                depositData._l2Gas,
+                depositData._data,
         }
     }
 }
