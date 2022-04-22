363d8acd7d1a2210a12bd544d5582532d5960026
shafu <selfouly@gmail.com>
2022-04-23 00:20:33 +0200

    kind of works

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,16 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import Item from './Item';
 import tokenList from '../tokenList';
 import { Button } from 'antd';
 
 export default function Home() {
+  const [checked, setChecked] = useState(Array(200).fill(true));
+  const [amount, setAmount] = useState([Array(200).fill(0)]);
+
+  useEffect(() => {}, [amount, checked]);
+
   return (
     <div>
       <div style={{ fontSize: '2rem' }}>The Multi Bridge</div>
-      {tokenList.tokens.map(token => {
+      {tokenList.tokens.map((token, i) => {
         // kovan
         if (token.chainId === 42) {
           return (
             <Item
+              id={i}
+              checked={checked}
+              setChecked={setChecked}
+              amount={amount}
+              setAmount={setAmount}
               name={token.name}
               address={token.address}
               logo={token.logoURI}
--- app/src/components/Item.jsx
+++ app/src/components/Item.jsx
@@ -1,7 +1,29 @@
 import { Input } from 'antd';
 import { Checkbox } from 'antd';
+import React, { useEffect } from 'react';
 
-export default function Item({ name, address, logo }) {
+export default function Item({
+  name,
+  address,
+  logo,
+  checked,
+  setChecked,
+  amount,
+  setAmount,
+  id,
+}) {
+  function setCh() {
+    let _checked = checked;
+    _checked[id] = false;
+    setChecked(_checked);
+  }
+
+  function setA(v) {
+    let _amount = amount;
+    _amount[id] = parseInt(v.target.value);
+    setAmount(_amount);
+  }
+
   return (
     <div>
       <div
@@ -12,14 +34,18 @@ export default function Item({ name, address, logo }) {
         }}
       >
         <div style={{ display: 'flex' }}>
-          <Checkbox />
+          <Checkbox checked={checked[id]} onChange={() => setCh()} />
           <span style={{ marginRight: '1rem', marginLeft: '2rem' }}>
             <img src={logo} width="25" height="25" />
           </span>
         </div>
         {name}
         <span style={{ marginLeft: '2rem' }}>
-          <Input placeholder="# of Tokens" />
+          <Input
+            placeholder="# of Tokens"
+            value={amount[id]}
+            onChange={v => setA(v)}
+          />
         </span>
       </div>
     </div>
