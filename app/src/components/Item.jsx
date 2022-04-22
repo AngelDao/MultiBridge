a98df69ce83148e40b39229f20131bb467cdec4e
shafu <selfouly@gmail.com>
2022-04-22 23:22:57 +0200

    checkbox

--- app/src/components/Item.jsx
+++ app/src/components/Item.jsx
@@ -1,12 +1,22 @@
 import { Input } from 'antd';
+import { Checkbox } from 'antd';
 
 export default function Item({ name, address, logo }) {
   return (
     <div>
-      <div style={{ display: 'flex', margin: '2rem' }}>
-        <span style={{ marginRight: '2rem' }}>
-          <img src={logo} width="25" height="25" />
-        </span>
+      <div
+        style={{
+          display: 'flex',
+          margin: '2rem',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ display: 'flex' }}>
+          <Checkbox />
+          <span style={{ marginRight: '1rem', marginLeft: '2rem' }}>
+            <img src={logo} width="25" height="25" />
+          </span>
+        </div>
         {name}
         <span style={{ marginLeft: '2rem' }}>
           <Input placeholder="# of Tokens" />
