fdab358e94ddcd5cca432aa582d36e9e025be185
shafu <selfouly@gmail.com>
2022-04-23 10:06:08 +0200

    connect wallet

---
+++ app/src/calldataUtils.js
@@ -0,0 +1,29 @@
+import tokenList from './tokenList';
+
+// test-net id: 69
+export function createCalldataArray(amount) {
+  let calldata = [];
+  const tokens = tokenList.tokens;
+
+  for (let i = 0; i < amount.length; i++) {
+    if (amount[i] > 0) {
+      calldata.push([
+        tokens[i].address,
+        getTestnetAddress(tokens[i].name).address,
+        amount[i],
+      ]);
+    }
+  }
+  console.log(calldata);
+  return calldata;
+}
+
+function getTestnetAddress(tokenName) {
+  const tokens = tokenList.tokens;
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i].name === tokenName && tokens[i].chainId === 69) {
+      return tokens[i];
+    }
+  }
+}
